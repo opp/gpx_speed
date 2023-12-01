a79de7b5667bafdea747393df34bbbf41b34e218
- <email@domain>
2023-12-01 10:35:02 -0500

    gpx speed with random hypen chance

---
+++ gpx_speed.py
@@ -0,0 +1,63 @@
+import time
+import os
+from random import randrange
+import configparser
+import ac
+import acsys
+
+pos = 0
+speed = 0
+l_speed = 0
+saved_epoch = 0
+hypen_chance = 99
+
+kmh = True
+mph = False
+
+show_text = ""
+get_speed = ""
+
+settings_default_ini_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "settings", "settings_defaults.ini")
+settings_ini_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "settings", "settings.ini")
+config      = configparser.ConfigParser()
+
+if not os.path.isfile(settings_ini_path): # create empty if not there
+    with open(settings_ini_path, "w", encoding="utf-8") as ini:
+        ini.write("")
+
+config.read(settings_ini_path)
+kmh = config.getboolean("GENERAL", "kmh")
+mph = config.getboolean("GENERAL", "mph")
+
+if kmh:
+    show_text = "km/h"
+    get_speed = acsys.CS.SpeedKMH
+if mph:
+    show_text = "MPH"
+    get_speed = acsys.CS.SpeedMPH
+
+def acMain(ac_version):
+    global l_speed
+    pos = ac.newApp("GPX Speed")
+    ac.setTitle(pos, "")
+    #ac.initFont(0, "retro_gaming", 0, 0)
+    #ac.setCustomFont(pos, "retro_gaming", 0, 0)
+    ac.setIconPosition(pos, -10000, -10000)
+    ac.setSize(pos, 70, 20)
+    ac.setBackgroundOpacity(pos, 1)
+    ac.drawBackground(pos, 1)
+    ac.drawBorder(pos, 0)
+    ac.setFontAlignment(pos, "center")
+    
+    l_speed = ac.addLabel(pos, "0 {}".format(show_text))
+    ac.setPosition(l_speed, 5, 0)
+    return "GPX Speed"
+    
+def acUpdate(deltaT):
+    global l_speed, speed, saved_epoch, hypen_chance
+    curr_epoch = int(time.time())
+    if curr_epoch != saved_epoch:
+        saved_epoch = curr_epoch
+        hypen_chance = randrange(100)
+        speed = int(ac.getCarState(0, get_speed))
+        ac.setText(l_speed, "---{}".format(show_text)) if hypen_chance <= 2 else ac.setText(l_speed, "{:03d}{}".format(speed, show_text))
